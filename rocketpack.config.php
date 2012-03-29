2aed8ba66e14fbc8edda5624bb28bd5c584fe8f6
Iain Dooley <iain@workingsoftware.com.au>
2012-03-29 12:09:01 +1100

    Bumping version numbres in RocketPack config file

--- rocketpack.config.php
+++ rocketpack.config.php
@@ -1,11 +1,11 @@
 <?php
     //note the second argument here is major, minor, patch
     //versions
-    rocketpack\Install::package('Fragmentify',array(0,1,0));
+    rocketpack\Install::package('Fragmentify',array(0,1,1));
 
     rocketpack\Dependencies::register(function()
     {
         rocketpack\Dependency::forPackage('Fragmentify')
-        ->add('Args',array(0,1,1))
+        ->add('Args',array(0,1,2))
         ->verify();
     });
