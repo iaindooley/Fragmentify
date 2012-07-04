f1a9b8165c53bda4e6a01098b076bc00c2ba83ed
Iain Dooley <iain@workingsoftware.com.au>
2012-07-04 15:11:12 +1000

    Bumped version number and that of dependencies

--- rocketpack.config.php
+++ rocketpack.config.php
@@ -1,11 +1,11 @@
 <?php
     //note the second argument here is major, minor, patch
     //versions
-    RocketPack\Install::package('https://github.com/iaindooley/Fragmentify',array(0,2,0));
+    RocketPack\Install::package('https://github.com/iaindooley/Fragmentify',array(0,2,1));
 
     RocketPack\Dependencies::register(function()
     {
         RocketPack\Dependency::forPackage('https://github.com/iaindooley/Fragmentify')
-        ->add('https://github.com/iaindooley/Args',array(0,2,0))
+        ->add('https://github.com/iaindooley/Args',array(0,2,1))
         ->verify();
     });
