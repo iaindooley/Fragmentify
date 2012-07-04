cd90c11486c95f2d2a6754a1d9232abca12ff5b6
Iain Dooley <iain@workingsoftware.com.au>
2012-07-04 14:31:27 +1000

    Modified to use new RocketPack namespacing

--- rocketpack.config.php
+++ rocketpack.config.php
@@ -1,11 +1,11 @@
 <?php
     //note the second argument here is major, minor, patch
     //versions
-    rocketpack\Install::package('https://github.com/iaindooley/Fragmentify',array(0,2,0));
+    RocketPack\Install::package('https://github.com/iaindooley/Fragmentify',array(0,2,0));
 
-    rocketpack\Dependencies::register(function()
+    RocketPack\Dependencies::register(function()
     {
-        rocketpack\Dependency::forPackage('https://github.com/iaindooley/Fragmentify')
+        RocketPack\Dependency::forPackage('https://github.com/iaindooley/Fragmentify')
         ->add('https://github.com/iaindooley/Args',array(0,2,0))
         ->verify();
     });
