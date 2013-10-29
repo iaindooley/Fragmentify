0ee139ace72fc113879c45f2ce28936775c0e469
Iain Dooley <iain@workingsoftware.com.au>
2013-10-30 08:20:08 +1100

    Updated rocketpack config format for new standalone rocketpack version

--- rocketpack.config.php
+++ rocketpack.config.php
@@ -1,11 +1,7 @@
 <?php
-    //note the second argument here is major, minor, patch
-    //versions
-    RocketPack\Install::package('https://github.com/iaindooley/Fragmentify',array(0,2,1));
-
-    RocketPack\Dependencies::register(function()
+    RocketPack\Dependency::register(function()
     {
-        RocketPack\Dependency::forPackage('https://github.com/iaindooley/Fragmentify')
-        ->add('https://github.com/iaindooley/Args',array(0,2,1))
-        ->verify();
+        RocketPack\Dependency::into(__DIR__.'/../')
+        ->add('https://github.com/iaindooley/Args')
+        ;
     });
